4fb3cf637363ccfab5945fbf4f5a380e97847afc
Andrei <bezzatechw1@gmail.com>
2025-11-02 00:38:48 +0700

    Created a menu template for each role.

---
+++ ViewModels/Waiter/WaiterMenuViewModel.cs
@@ -0,0 +1,29 @@
+using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+using cafeInformationSystem.Models.AuthService;
+using Avalonia;
+using cafeInformationSystem.Views.Shared;
+using Avalonia.Controls.ApplicationLifetimes;
+using cafeInformationSystem.Models.MediaService;
+using System;
+using Avalonia.Platform;
+
+namespace cafeInformationSystem.ViewModels.Waiter;
+
+public class WaiterMenuViewModel : ViewModelBase
+{
+    public WaiterMenuViewModel()
+    {
+        //LogoutCommand = new RelayCommand(ExecuteLogout);
+    }
+
+
+    //public ICommand LogoutCommand { get; }
+
+    // private void ExecuteLogout()
+    // {
+        
+    // }
+
+}
---
+++ Views/Chef/ChefMenuWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace cafeInformationSystem.Views.Chef;
+
+public partial class ChefMenuWindow : Window
+{
+    public ChefMenuWindow()
+    {
+        InitializeComponent();
+    }
+}
